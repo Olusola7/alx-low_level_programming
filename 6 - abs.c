cb8bc9024b5b10e5a209fec4998206543842b48f
olushola ajakaiye <110997123+Olusola7@users.noreply.github.com>
2022-09-18 06:49:26 -0700

    Create 6 - abs.c
    Signed-off-by: olushola ajakaiye <110997123+Olusola7@users.noreply.github.com>

---
+++ 6 - abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* _abs - value absolute
+*
+*@n: the int to print
+* return: always 0.
+*/
+int _abs(int n)
+{
+  if (n < 0)
+  {
+    return (n * (-1));
+  }
+  return (0);
+}
+eles
+{return (n);
+}
