132a93afa1023b6ee5d1a8a6450d1c3fe2d49ea7
olushola ajakaiye <110997123+Olusola7@users.noreply.github.com>
2022-09-20 07:14:59 -0700

    Create 102 - fibonacci.c
    Signed-off-by: olushola ajakaiye <110997123+Olusola7@users.noreply.github.com>

---
+++ 102 - fibonacci.c
@@ -0,0 +1,49 @@
+#include <stdio.h> 
+
+/** 
+
+*main -  prints the first 50 fibonacci numbers, starting with 1 and 2 
+
+* followed by a new line 
+
+* return: always 0 ( success) 
+
+*/ 
+
+Int main (void) 
+
+{ 
+
+               Long int I,  j  ,  k ,  next; 
+
+                J = 1; 
+
+               K = 2; 
+
+               For   (I  = 1;   i <= 50;  ++I) 
+
+{ 
+
+                              If   (j  != 20365011074) 
+
+{ 
+
+                                           Printf(“%1d\n” ,j); 
+
+}eles 
+
+{ 
+
+                                          Printf(“%1d\n” , j); 
+
+} 
+
+Nest = j  +  k ; 
+
+J = k ; 
+
+K  = next; 
+
+ } 
+
+Return (0) 
